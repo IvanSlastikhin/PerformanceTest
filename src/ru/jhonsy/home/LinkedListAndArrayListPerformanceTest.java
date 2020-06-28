2a7c2dcb6e44be27adb2b80cf45b101b3c6f1784
Ivan Slastikhin <stopgo89@gmail.com>
2020-06-28 16:09:13 +0300

    Performance test for operations under LinkerList and ArrayList is added

---
+++ src/ru/jhonsy/home/LinkedListAndArrayListPerformanceTest.java
@@ -0,0 +1,83 @@
+package ru.jhonsy.home;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @Author: Ivan Slastikhin
+ */
+public class LinkedListAndArrayListPerformanceTest {
+
+    private static final int LIST_SIZE = 1000000;
+    private static final int NUMBER_OF_ITERATIONS = 10000;
+
+    private static List<Integer> linkedList = new LinkedList<>();
+    private static List<Integer> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        //Производительность вставки в конец списка
+
+        long startTime = System.currentTimeMillis();
+        fillListWithRandomValues(linkedList, random);
+        System.out.println("Adding " + LIST_SIZE + " random values to LinkedList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+
+        startTime = System.currentTimeMillis();
+        fillListWithRandomValues(arrayList, random);
+        System.out.println("Adding " + LIST_SIZE + " random values to ArrayList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+        System.out.println("==============================================================");
+
+        //Производительность получения значений на i-ой позиции
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            linkedList.get(i);
+        }
+        System.out.println("Getting " + NUMBER_OF_ITERATIONS + " values at position i from LinkedList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            arrayList.get(i);
+        }
+        System.out.println("Getting " + NUMBER_OF_ITERATIONS + " values at position i from ArrayList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+        System.out.println("==============================================================");
+
+        //Производительность удалений элементов с i-ой позиции
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            linkedList.remove(i);
+        }
+        System.out.println("Removing " + NUMBER_OF_ITERATIONS + " values at position i from LinkedList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+            arrayList.remove(i);
+        }
+        System.out.println("Removing " + NUMBER_OF_ITERATIONS + " values at position i from ArrayList took: "
+                + ((double) (System.currentTimeMillis() - startTime)) + " ms");
+
+        System.out.println("==============================================================");
+
+    }
+
+    private static void fillListWithRandomValues(List<Integer> list, Random random){
+        for (int i = 0; i < LIST_SIZE; i++) {
+            list.add(random.nextInt(Integer.MAX_VALUE));
+        }
+    }
+}
